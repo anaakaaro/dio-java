20d3faefb4bccebeb0a79be7d1454c8b12a79082
anaakaaro <aanaakaarolina@gmail.com>
2025-05-27 19:03:13 -0300

    inserção do R$ em saldo

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -19,7 +19,7 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o saldo da conta!");
         saldo = scanner.nextDouble();
 
-        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque\".");
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo R$" + saldo + " já está disponível para saque\".");
         scanner.close();
     }
 }
