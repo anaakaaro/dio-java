223c02b589a97729ad357f6bdfdacf66aec515f7
Ana Carolina <aanaakaarolina@gmail.com>
2025-06-13 08:44:28 -0300

    criação classe iphone

---
+++ desafio-poo/src/Iphone.java
@@ -0,0 +1,39 @@
+public class Iphone implements AparelhoTelefonico, NavegadorInternet, ReprodutorMusical {
+
+    @Override
+    public void tocar() {
+    }
+
+    @Override
+    public void pausar() {
+    }
+
+    @Override
+    public void selecionarMusica(String musica) {
+    }
+
+    @Override
+    public void exibirPagina(String url) {
+    }
+
+    @Override
+    public void adicionarNovaAba() {
+    }
+
+    @Override
+    public void atualizarPagina() {
+    }
+
+    @Override
+    public void ligar(String numero) {
+    }
+
+    @Override
+    public void atender() {
+    }
+
+    @Override
+    public void iniciarCorreioVoz() {
+    }
+
+}
---
+++ desafio-poo/src/IphoneCarol.java
@@ -0,0 +1,18 @@
+public class IphoneCarol {
+    public static void main(String[] args) {
+        Iphone iphoneCarol = new Iphone();
+
+        iphoneCarol.ligar(null);
+        iphoneCarol.atender();
+        iphoneCarol.iniciarCorreioVoz();
+
+        iphoneCarol.exibirPagina(null);
+        iphoneCarol.adicionarNovaAba();
+        iphoneCarol.atualizarPagina();
+
+        iphoneCarol.selecionarMusica(null);
+        iphoneCarol.tocar();
+        iphoneCarol.pausar();
+
+    }
+}
